b0fda851a5d3868db3e883b2ea7edfca295e0f27
Scott Crowder <scrowder@gmail.com>
2025-01-14 11:55:46 -0600

    Initial Commit

---
+++ BookWormCD/BookWormCDApp.swift
@@ -0,0 +1,20 @@
+//
+// File: BookWormCDApp.swift
+// Project: BookWormCD
+// 
+// Created by SCOTT CROWDER on 1/14/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct BookWormCDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
